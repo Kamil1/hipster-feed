a69b5c0bb506f7df697c6ffd769f93bc6b64aa91
Kamil Khan <kamil.z.khan@gmail.com>
2015-09-19 01:18:43 -0700

    running with auth_token as argument now renders user timeline

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 #! /usr/bin/env node
 var https = require('https');
+var ImageToAscii = require("image-to-ascii");
 
 var userArgs = process.argv.slice(2);
 
-var clientID = userArgs[0];
-var clientSecret = userArgs[1];
-var access_token = userArgs[2];
+var access_token = userArgs[0];
 
 https.get('https://api.instagram.com/v1/users/self/feed?access_token=' + access_token, function(res) {
   var body = '';
@@ -17,7 +16,10 @@ https.get('https://api.instagram.com/v1/users/self/feed?access_token=' + access_
   res.on('end', function() {
   	var response = JSON.parse(body);
   	for (var i = 0; i < response["data"].length; i++) {
-  		url=response["data"][i]["images"]["low_resolution"]["url"];
+  		url=response["data"][i]["images"]["standard_resolution"]["url"];
+  		ImageToAscii(url, function(err, converted) {
+  			console.log(err || converted);
+  		});
   	}
   });
 
